6e42d91ec3c0ad5008706615d4d553a2814610e9
infochibafukushi-dotcom <info.chibafukushi@gmail.com>
2026-03-19 07:21:26 +0900

    Update public-app.js

--- public-app.js
+++ public-app.js
@@ -1,5 +1,55 @@
+const STORAGE_KEY = "reservations_v2";
+
+function getData() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+}
+
+function saveData(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function render() {
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+
+  const data = getData();
+
+  data.forEach((r, i) => {
+    const li = document.createElement("li");
+    li.textContent = r.name + " / " + r.date;
+
+    const btn = document.createElement("button");
+    btn.textContent = "削除";
+    btn.onclick = () => {
+      data.splice(i, 1);
+      saveData(data);
+      render();
+    };
+
+    li.appendChild(btn);
+    list.appendChild(li);
+  });
+}
+
 function initApp() {
-  console.log("public app start");
+  document.getElementById("title").innerText = CONFIG.siteName;
+
+  document.getElementById("form").onsubmit = function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const date = document.getElementById("date").value;
+
+    const data = getData();
+    data.push({ name, date });
+
+    saveData(data);
+
+    this.reset();
+    render();
+  };
+
+  render();
 }
 
 initApp();
