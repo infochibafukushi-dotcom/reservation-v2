59df2bb4abd944fd51226454954743482ad8f272
infochibafukushi-dotcom <info.chibafukushi@gmail.com>
2026-04-07 22:42:34 +0900

    Ensure reservation success always hits fixed notify URL

--- admin.calendar.js
+++ admin.calendar.js
@@ -295,6 +295,7 @@ function bindAdminGridDelegation(){
             minute: minute
           });
           await adminRefreshVisibleWindow();
+          try{ adminClearPublicClientCache(); }catch(_){}
         }, '枠を更新中...');
       }
 
@@ -322,6 +323,7 @@ function bindAdminGridDelegation(){
             isBlocked: nextState
           });
           await adminRefreshVisibleWindow();
+          try{ adminClearPublicClientCache(); }catch(_){}
         }, '日単位ブロック更新中...');
       }
 
@@ -349,6 +351,7 @@ function bindAdminGridDelegation(){
             isBlocked: nextState
           });
           await adminRefreshVisibleWindow();
+          try{ adminClearPublicClientCache(); }catch(_){}
         }, '時間帯一括更新中...');
       }
     }catch(err){
